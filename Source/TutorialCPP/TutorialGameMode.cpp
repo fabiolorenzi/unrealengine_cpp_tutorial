4cc1f6bae11a3609c1900b32a2eb4bee3ff142c5
fabiolorenzi <fabiolorenzi13975@gmail.com>
2023-09-15 15:26:11 +0100

    developed spawning system

--- Source/TutorialCPP/TutorialGameMode.cpp
+++ Source/TutorialCPP/TutorialGameMode.cpp
@@ -2,4 +2,33 @@
 
 
 #include "TutorialGameMode.h"
+#include "GameFramework/Actor.h"
 
+ATutorialGameMode::ATutorialGameMode()
+{
+    PrimaryActorTick.bCanEverTick = true;
+}
+
+void ATutorialGameMode::BeginPlay()
+{
+    Super::BeginPlay();
+
+    FTimerHandle UnusedHandle;
+    GetWorldTimerManager().SetTimer(UnusedHandle, this, &ATutorialGameMode::SpawnPlayerRecharge, FMath::RandRange(2, 5), true);
+}
+
+void ATutorialGameMode::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+void ATutorialGameMode::SpawnPlayerRecharge()
+{
+    float RandX = FMath::RandRange(SpawnXMin, SpawnXMax);
+    float RandY = FMath::RandRange(SpawnYMin, SpawnYMax);
+
+    FVector SpawnPosition = FVector(RandX, RandY, SpawnZ);
+    FRotator SpawnRotation = FRotator(0.0f, 0.0f, 0.0f);
+
+    GetWorld()->SpawnActor(PlayerRecharge, &SpawnPosition, &SpawnRotation);
+}
--- Source/TutorialCPP/TutorialGameMode.h
+++ Source/TutorialCPP/TutorialGameMode.h
@@ -14,4 +14,23 @@ class TUTORIALCPP_API ATutorialGameMode : public AGameMode
 {
 	GENERATED_BODY()
 	
+	ATutorialGameMode();
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+		TSubclassOf<APawn> PlayerRecharge;
+
+	float SpawnZ = 500.0f;
+	
+	UPROPERTY(EditAnywhere)
+		float SpawnXMin;
+	UPROPERTY(EditAnywhere)
+		float SpawnXMax;
+	UPROPERTY(EditAnywhere)
+		float SpawnYMin;
+	UPROPERTY(EditAnywhere)
+		float SpawnYMax;
+
+	void SpawnPlayerRecharge();
 };
